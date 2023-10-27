6ff0d88e3c3489443a87d8a1f30f16ab47b036d1
Voliwey <125397102+Voliwey@users.noreply.github.com>
2023-10-27 15:19:40 +0000

    Deploying to gh-pages from @ Voliwey/goit-js-hw-09@3c66ab051937ec8ffc45afb4b6b21ce5a160a97f 🚀

---
+++ 01-color-switcher.a5b85f08.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(function(){e.disabled=!0,o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a5b85f08.js.map
